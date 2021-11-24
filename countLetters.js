12d1a6f6ad828cf8ef4fce6e0fc7dc0afd6aabee
GHadeer Khader <qods_a@yahoo.com>
2021-11-24 10:13:32 +0000

    countLetters updated

---
+++ countLetters.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  console.assert(actual===expected, {actual:expected}, 'failed')
+};
+
+const countLetters = function(string){
+  let output = {};
+  for(let letter of string){
+    if(output[letter] == undefined){
+      output[letter] = 1;
+      }
+      else{
+        output[letter] += 1;
+      } 
+  }
+  return output;
+};
