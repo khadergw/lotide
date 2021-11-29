8796ae6ff52ea1d16d13a3a648588052eff69f59
GHadeer Khader <qods_a@yahoo.com>
2021-11-29 04:44:13 +0000

    refractored by mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,7 +2,16 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 const middle = require('../middle');
 
+const assert = require('chai').assert;
 
-console.log(middle([2,3,4,0]));
-console.log(middle([2,3,4]));
-console.log(middle([2,3]));
+
+// console.log(middle([2,3,4,0]));
+// console.log(middle([2,3,4]));
+// console.log(middle([2,3]));
+
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+});
